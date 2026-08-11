8f5fbeb43efd49f6451bfe1bf77adc2947425bcc
Matysse <mathis.jung@blgcloud.com>
2026-08-11 11:39:19 +0200

    WIP Schema refacto

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,7 +2,7 @@ export interface UnresolvedSchema {
     description?: string;
     type?: string;
     format?: string;
-    default?: string;
+    default?: any;
 
     const?: string;
     enum?: string[];
@@ -19,14 +19,6 @@ export interface UnresolvedSchema {
     examples?: any[];
 
     $ref?: string;
-
-    // TODO:
-    // https://spec.openapis.org/oas/v3.2.0.html#generic-data-structure-model
-    // $id, $defs, $dynamicDefs, $dynamicAnchor
-    // discriminator
-    // allOf
-    // anyOf
-    // oneOf
 }
 
 export type ResolvedSchema = UnresolvedSchema & {$ref?: never}
